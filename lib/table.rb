9472d76b793606592181c52d351a7c26b78fcf08
Nick Butler <nick@codeindulgence.com>
2014-04-30 21:11:05 +1000

    Methods for adjacent cells

--- lib/table.rb
+++ lib/table.rb
@@ -1,11 +1,44 @@
 class Table
 
+  # Parameters
+  # width: Fixnum
+  # height: Fixnum
   def initialize width, height
     @width = width
     @height = height
   end
 
+  # No arguments
+  # Returns String of format "<width>x<height>"
   def size
     "#{@width}x#{@height}"
+  end
+
+  # Accepts x and y coordinates
+  # Returns coordinates array [x, y]
+  # or nil if out of bounds
+  def north_of x, y
+    [x, y + 1] unless y + 1 == @height
+  end
+
+  # Accepts x and y coordinates
+  # Returns coordinates array [x, y]
+  # or nil if out of bounds
+  def east_of x, y
+    [x + 1, y] unless x + 1 == @width
+  end
+
+  # Accepts x and y coordinates
+  # Returns coordinates array [x, y]
+  # or nil if out of bounds
+  def south_of x, y
+    [x, y - 1] unless y - 1 < 0
+  end
+
+  # Accepts x and y coordinates
+  # Returns coordinates array [x, y]
+  # or nil if out of bounds
+  def west_of x, y
+    [x - 1, y] unless x - 1 < 0
   end
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -11,5 +11,22 @@ describe Table do
       assert_equal table.size, '5x5'
     end
   end
+
+  describe "when asked about adjacent cells" do
+    it "returns correct results" do
+      table = Table.new 5, 5
+      assert_equal table.north_of(0, 0), [0, 1]
+      assert_equal table.north_of(0, 4), nil
+
+      assert_equal table.east_of(0, 0), [1, 0]
+      assert_equal table.east_of(4, 0), nil
+
+      assert_equal table.south_of(4, 4), [4, 3]
+      assert_equal table.south_of(0, 0), nil
+
+      assert_equal table.west_of(4, 0), [3, 0]
+      assert_equal table.west_of(0, 0), nil
+    end
+  end
 end
 
