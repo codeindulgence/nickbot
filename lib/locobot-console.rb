4a4de907ef6a78a3d011c13d50ce721318b245d5
Nick Butler <nick@codeindulgence.com>
2014-04-30 23:19:23 +1000

    CLI now applies our table

--- lib/locobot-console.rb
+++ lib/locobot-console.rb
@@ -2,7 +2,10 @@ require 'highline/import'
 require 'debugger'
 
 require File.expand_path('../../lib/locobot',  __FILE__)
+require File.expand_path('../../lib/table',  __FILE__)
+
 @locobot = Locobot::Robot.new
+@locobot.table = Table.new 5, 5
 
 def prompt
   command = ask("<%= color('LOCOBOT AWAITING COMMAND> ', BLUE) %>") { |q| q.case = :upcase }
